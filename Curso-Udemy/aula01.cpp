58941b7f56ff4b6070fcaa0c93573f275394f51e
BrunoBianchi <bruno.raiadobian@gmail.com>
2023-03-02 11:26:22 -0300

    C++ Rep

---
+++ Curso-Udemy/aula01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip> 
+using namespace std;
+int main() {
+    // declaracao de todas as variaveis;
+    int a = 0;
+    double b = 1.3123 ;
+    char c = 'c';
+    string d = "Ana Maria";
+
+   cout << a << endl;
+   cout << fixed << setprecision(1) << b << endl ;
+   cout << c << endl;
+   cout << d << endl;
+    return 0 ;
+}
